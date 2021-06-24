f1ee3572739de1150986a76dd5491fde8cc56893
Justin Hung <justin@experience.digital>
2021-06-24 17:11:20 +1000

    added getting started sentry code and node types

--- index.ts
+++ index.ts
@@ -1,1 +1,26 @@
-console.log('hello world')
+import * as Sentry from '@sentry/node'
+import * as Tracing from '@sentry/tracing'
+
+Sentry.init({
+  dsn: "https://8b86687cbf9644ef8a028d3853c684e9@o109215.ingest.sentry.io/5831479",
+
+  // Set tracesSampleRate to 1.0 to capture 100%
+  // of transactions for performance monitoring.
+  // We recommend adjusting this value in production
+  tracesSampleRate: 1.0,
+});
+
+const transaction = Sentry.startTransaction({
+  op: "test",
+  name: "My First Test Transaction",
+});
+
+setTimeout(() => {
+  try {
+    foo();
+  } catch (e) {
+    Sentry.captureException(e);
+  } finally {
+    transaction.finish();
+  }
+}, 99);

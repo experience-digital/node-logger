2133f690212af8b0a8a21bea286690c14f6848d5
Justin Hung <justin@experience.digital>
2021-06-30 17:59:11 +1000

    fix: updated tsconfig compiler options

--- build/index.js
+++
@@ -1,6 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var Log = require("./logger");
-Log["default"].log("https://8b86687cbf9644ef8a028d3853c684e9@o109215.ingest.sentry.io/5831479", 'node', 'node test');
-Log["default"].log("https://f76cf12cbd724f9ba365ab5efed8eddb@o109215.ingest.sentry.io/5831533", 'vue', 'vue test');
-Log["default"].log("https://730f4cc255df4fb5809d64290adbfd55@o109215.ingest.sentry.io/5833138", 'js', 'js test');
